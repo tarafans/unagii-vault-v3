4796bf4589032e9e0d5aa0b604c9a40470ca4ba6
chanleyou <chanleyou@gmail.com>
2023-02-07 06:04:24 +0800

    updated scripts

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity 0.8.9;
+pragma solidity ^0.8.9;
 
 import 'forge-std/console.sol';
 
@@ -49,6 +49,52 @@ contract Deploy is Script {
 
 		wethStrategy.setAdmin(multisig);
 		wethStrategy.nominateOwnership(timeLock);
+
+		vm.stopBroadcast();
+	}
+}
+
+contract Setup is Script {
+	address multisig = vm.envAddress('MULTISIG_ADDRESS');
+	address timeLock = vm.envAddress('TIMELOCK_ADDRESS');
+
+	function run() external {
+		vm.startBroadcast(vm.envUint('PRIVATE_KEY'));
+
+		WethStrategyConvexStEth wethStrat = WethStrategyConvexStEth(
+			payable(vm.envAddress('WETH_STRATEGY_CONVEX_STETH'))
+		);
+		wethStrat.setAdmin(multisig);
+		wethStrat.nominateOwnership(timeLock);
+
+		Vault wethVault = Vault(vm.envAddress('WETH_VAULT'));
+		wethVault.nominateOwnership(timeLock);
+
+		vm.stopBroadcast();
+	}
+}
+
+contract Deposit is Script {
+	ERC20 constant USDC = ERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+	ERC20 constant WETH9 = ERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+
+	address me = 0x7c5D6df0646aD1Dc77FFB52E2430bE779729ed69;
+
+	function run() external {
+		vm.startBroadcast(vm.envUint('MY_PRIVATE_KEY'));
+
+		Vault wethVault = Vault(vm.envAddress('WETH_VAULT'));
+
+		uint256 wethBal = WETH9.balanceOf(me);
+		WETH9.approve(address(wethVault), wethBal);
+
+		wethVault.deposit(wethBal, me);
+
+		Vault usdcVault = Vault(vm.envAddress('USDC_VAULT'));
+		uint256 usdcBal = USDC.balanceOf(me);
+		USDC.approve(address(usdcVault), usdcBal);
+
+		usdcVault.deposit(usdcBal, me);
 
 		vm.stopBroadcast();
 	}
